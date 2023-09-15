e08568e079050c57c24c70d398e8ddbae7004700
DESKTOP-7Q0HBGC\HLC_2021 <hoangvanquan2002.unitydevelop@gmail.com>
2023-09-15 17:47:16 +0700

    save

---
+++ Assets/_Game/Scripts/SaveData/Singletons.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+	private static T _instance;
+
+	/// <summary>
+	/// This will be create a new game object if instance is null
+	/// So if you want to check this null, use bool 'Initialized' instead.
+	/// </summary>
+	public static T Instance {
+		get {
+			if (_instance != null) return _instance;
+			_instance = (new GameObject(typeof(T).Name)).AddComponent<T>();
+			return _instance;
+		}
+	}
+
+	/// <summary>
+	/// Use for checking whether Instance is null or not. 
+	/// <para>Use "if (Initialized)" instead of "if (Instance != null)"</para>
+	/// </summary>
+	public static bool Initialized => _instance != null;
+
+	protected virtual bool DontDestroy => false;
+
+	protected virtual void OnAwake() { }
+
+	private void Awake() {
+		if (_instance == null) {
+			_instance = this as T;
+		}
+		else if (_instance != this as T) {
+			Destroy(gameObject);
+			return;
+		}
+
+		if (DontDestroy) DontDestroyOnLoad(gameObject);
+
+		OnAwake();
+	}
+
+
+	///<summary>Call 'T.Instance.Preload()' at the first application script to preload the service.</summary>
+	public virtual void Preload() { }
+}
+
+public class SingletonBehaviourDontDestroy<T> : SingletonBehaviour<T> where T : MonoBehaviour {
+	protected override bool DontDestroy => true;
+}
---
+++ Assets/_Game/Scripts/SaveData/UserDataController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UserDataController : SingletonBehaviourDontDestroy<UserDataController>
+{
+    Dictionary<string, object> datas = new();
+
+    public T GetData<T>(string key,out bool isNew) where T: IUserData, new()
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            T t = JsonUtility.FromJson<T>(PlayerPrefs.GetString(key));
+            isNew = false;
+            return t;
+        }
+        else
+        {
+            isNew = true;
+            return new T();
+        }
+    }
+
+    public void SetData<T>(string key, T value, bool forceSaved = false) where T: IUserData
+    {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(value));
+
+        if(forceSaved || true) // temprorary here
+        {
+            
+        }
+    }
+}
+public class UserDataKeys
+{
+    public const string USER_PROGRESSION = "data.user_prog";
+}
+
+public interface IUserData
+{
+
+}
+
+
+
+[Serializable]
+public class ShopInfoCharacterAds
+{
+    public string id;
+    public int currentAdsWatched;
+    public int amountAdsNeed;
+}
+
